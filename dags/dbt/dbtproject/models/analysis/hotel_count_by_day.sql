3f5601739b7d81aaa92775f305884feadde53f25
Alfred <alfredsnk@gmail.com>
2024-11-29 21:52:47 -0400

    more documentaion changes

--- dags/dbt/dbtproject/models/analysis/hotel_count_by_day.sql
+++ dags/dbt/dbtproject/models/analysis/hotel_count_by_day.sql
@@ -1,8 +1,9 @@
+-- Seleccionamos la fecha de reserva, el hotel, y contamos el número de reservas (ID) realizadas
 SELECT
-  BOOKING_DATE,
-  HOTEL,
-  COUNT(ID) as count_bookings
-FROM {{ ref('prepped_data') }}
+  BOOKING_DATE, -- Fecha en la que se realizó la reserva
+  HOTEL,        -- Nombre o identificador del hotel
+  COUNT(ID) as count_bookings -- Número total de reservas por combinación de hotel y fecha
+FROM {{ ref('prepped_data') }} -- Tabla o vista preprocesada en dbt
 GROUP BY
-  BOOKING_DATE,
-  HOTEL
+  BOOKING_DATE, -- Agrupamos por fecha de reserva
+  HOTEL         -- Agrupamos por hotel
--- dags/dbt/dbtproject/models/analysis/thirty_day_avg_cost.sql
+++ dags/dbt/dbtproject/models/analysis/thirty_day_avg_cost.sql
@@ -1,11 +1,14 @@
+-- Seleccionamos los datos de reservas, calculando el promedio móvil de 30 días y la diferencia con el costo actual
 SELECT
-  BOOKING_DATE,
-  HOTEL,
-  COST,
+  BOOKING_DATE, -- Fecha en la que se realizó la reserva
+  HOTEL,        -- Nombre o identificador del hotel
+  COST,         -- Costo de la reserva
+  -- Calculamos el promedio móvil de 30 días usando una ventana sobre las filas de los 29 días anteriores más el día actual
   AVG(COST) OVER (
     ORDER BY BOOKING_DATE ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
-  ) as "30_DAY_AVG_COST",
-  COST -   AVG(COST) OVER (
+  ) as "30_DAY_AVG_COST", -- Promedio móvil del costo en los últimos 30 días (incluyendo el actual)
+  -- Calculamos la diferencia entre el costo actual y el promedio móvil de 30 días
+  COST - AVG(COST) OVER (
     ORDER BY BOOKING_DATE ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
-  ) as "DIFF_BTW_ACTUAL_AVG"
-FROM {{ ref('prepped_data') }}
+  ) as "DIFF_BTW_ACTUAL_AVG" -- Diferencia entre el costo actual y el promedio móvil
+FROM {{ ref('prepped_data') }} -- Tabla o vista preprocesada en dbt
